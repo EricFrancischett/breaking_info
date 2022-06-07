ad468d5f8534a48730ef6775cf22186bf049cb11
EricFrancischett <ericmellof@gmail.com>
2022-06-07 13:21:47 -0300

    login_page created

---
+++ lib/core/constans/auth_api_routes.dart
@@ -0,0 +1,7 @@
+class AuthApiRoutes {
+  static const baseUrl = "https://academy-auth.herokuapp.com";
+  static const loginApiRoute = "$baseUrl/login";
+  static const registerApiRoute = "$baseUrl/register";
+  static const updateApiRoute = "$baseUrl/update";
+  static const deleteUserApiRoute = "$baseUrl/delete-user";
+}
---
+++ lib/core/constans/breaking_bad_api_routes.dart
@@ -0,0 +1,8 @@
+class BreakingBadApiRoute {
+  static const baseUrl = "https://www.breakingbadapi.com/api/";
+  static const allCharactrsApiRoute = "$baseUrl/characters?category=Breaking+Bad";
+  static const searchByNameApiRoute = "$baseUrl/characters?name=";
+  static const allEpisodesApiRoute = "$baseUrl/episodes?series=Breaking+Bad";
+  static const deathByNameApiRoute = "$baseUrl/death?name=";
+  static const deathCountByNameApiRoute = "$baseUrl/death-count?name=";
+}
